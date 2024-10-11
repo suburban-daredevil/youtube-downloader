2194b1a83e38b7d39fe4306ee89e2f9ecfeaab58
Sriram S <sriramdav1729@gmail.com>
2024-10-11 16:17:21 -0400

    feat: updated link to Beat It

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from download_video import download_video
 
 # specify the URL of the video
-url = 'https://youtu.be/DhCYfOiDUEw'
+url = 'https://www.youtube.com/watch?v=oRdxUFDoQe0'
 
 # specify the path of the directory where you want your video to be saved
 directory_path = 'D:/Songs'
